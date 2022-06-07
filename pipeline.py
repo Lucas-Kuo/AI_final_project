6cf15e7385a015b6be2fe9175b2014347a194d6b
Lucas Kuo <74166753+Lucas-Kuo@users.noreply.github.com>
2022-06-07 23:47:35 +0800

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -7,36 +7,35 @@ import config
 
 
 def load_images(imagePath):
-	# read the image from disk, decode it, resize it, and scale the
-	# pixels intensities to the range [0, 1]
-	image = tf.io.read_file(imagePath)
+    # read the image from disk, decode it, resize it, and scale the
+    # pixels intensities to the range [0, 1]
+    image = tf.io.read_file(imagePath)
     image = tf.image.decode_jpeg(image, channels=3)
-	image = tf.image.resize(image, config.IMG_SIZE) / 255.0
-	# grab the label and encode it
-	label = tf.strings.split(imagePath, os.path.sep)[-2]
-	oneHot = label == config.CLASSES # [0, 0, ..., 0, 1, 0, ..., 0]
-	encodedLabel = tf.argmax(oneHot)
-	# return the image and the integer encoded label
-	return (image, encodedLabel)
+    image = tf.image.resize(image, config.IMG_SIZE) / 255.0
+    # grab the label and encode it
+    label = tf.strings.split(imagePath, os.path.sep)[-2]
+    oneHot = label == config.CLASSES # [0, 0, ..., 0, 1, 0, ..., 0]
+    encodedLabel = tf.argmax(oneHot)
+    # return the image and the integer encoded label
+    return (image, encodedLabel)
 
 def load_images_test(imagePath):
     # read the image from disk, decode it, resize it, and scale the
 	# pixels intensities to the range [0, 1]
-	image = tf.io.read_file(imagePath)
-	image = tf.image.decode_png(image, channels=3)
-#   image = tf.image.decode_jpeg(image, channels=3)
-	image = tf.image.resize(image, config.IMG_SIZE) / 255.0
-	# return the image and the integer encoded label
-	return (image, )
+    image = tf.io.read_file(imagePath)
+    image = tf.image.decode_jpeg(image, channels=3)
+    image = tf.image.resize(image, config.IMG_SIZE) / 255.0
+    # return the image and the integer encoded label
+    return (image, )
 
 @tf.function
 def augment(image, label):
-	# perform random horizontal and vertical flips
+    # perform random horizontal and vertical flips
 # 	image = tf.image.random_flip_up_down(image) # This doesn't make sense for this task
-	image = tf.image.random_flip_left_right(image)
+    image = tf.image.random_flip_left_right(image)
     image = tf.image.random_brightness(image, 0.25)
-	# return the image and the label
-	return (image, label)
+    # return the image and the label
+    return (image, label)
 
 def load_dataset(subset="training"): # for training and validation
     print(f"[INFO] loading image paths for {subset} subset...")
@@ -46,7 +45,7 @@ def load_dataset(subset="training"): # for training and validation
         imagePaths = list(paths.list_images(config.VAL_PATH))
     else:
         raise ValueError(f"Parameter \'subset\' accepts either \'training\' or \'validation\', but \'{subset}\' was given.")
-        
+
     print("[INFO] creating a tf.data input pipeline..")
     dataset = tf.data.Dataset.from_tensor_slices(imagePaths)
     if subset=="training":
