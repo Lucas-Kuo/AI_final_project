edc6aec514607a2c2b97681fbbcb4b6565538fe0
Lucas Kuo <74166753+Lucas-Kuo@users.noreply.github.com>
2022-06-11 03:50:15 +0800

    Update config.py

--- config.py
+++ config.py
@@ -26,9 +26,9 @@ CLASSES = []
 FILENAME_CLASS_PAIR = {}
 
 for index, row in dataframe.iterrows():
-    FILENAME_CLASS_PAIR[row["image"]] = row["individual_id"]
-    if row["individual_id"] not in CLASSES:
-        CLASSES.append(row["individual_id"])
+    FILENAME_CLASS_PAIR[row["image"]] = row["species"]
+    if row["species"] not in CLASSES:
+        CLASSES.append(row["species"])
 
 CLASSES = np.array(sorted(CLASSES))
         
